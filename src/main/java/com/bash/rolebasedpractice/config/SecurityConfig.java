9b2b275254caec66401b76faca7d5856ce8e1b31
Bashir Muhammed <bashmufol@gmail.com>
2025-07-20 14:50:55 +0100

    updated

---
+++ src/main/java/com/bash/rolebasedpractice/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.bash.rolebasedpractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/register", "/login", "/", "/products").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/admin/**", "/manager/**").hasAnyRole("ADMIN", "MANAGER")
+                        .requestMatchers(HttpMethod.POST, "/products/").hasAuthority("ADMIN_CREATE")
+                        .requestMatchers(HttpMethod.DELETE, "/products/").hasAuthority("ADMIN_DELETE")
+
+    }
+}
