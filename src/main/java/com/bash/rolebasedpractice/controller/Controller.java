02f2d4b1dc03c40af1dfde5bf10d45ae96eacc0c
Bashir Muhammed <bashmufol@gmail.com>
2025-07-20 15:18:14 +0100

    updated

--- src/main/java/com/bash/rolebasedpractice/controller/Controller.java
+++ src/main/java/com/bash/rolebasedpractice/controller/Controller.java
@@ -1,8 +1,28 @@
 package com.bash.rolebasedpractice.controller;
 
+import com.bash.rolebasedpractice.model.Users;
+import com.bash.rolebasedpractice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class Controller {
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
 
+    @GetMapping("/")
+    public String getHome(){
+        return "Welcome to my project home page!";
+    }
+
+    @PostMapping("/register")
+    public Users registerUser(@RequestBody Users user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userService.registerUser(user);
+    }
 }
--- src/main/java/com/bash/rolebasedpractice/service/UserService.java
+++ src/main/java/com/bash/rolebasedpractice/service/UserService.java
@@ -1,4 +1,19 @@
 package com.bash.rolebasedpractice.service;
 
+import com.bash.rolebasedpractice.model.Users;
+import com.bash.rolebasedpractice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+
+    public Users registerUser(@RequestBody Users user) {
+        return userRepository.save(user);
+    }
+
 }
